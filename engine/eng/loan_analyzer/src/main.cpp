d564cc6b69a98bcfffd05fc8e674cd389d754911
jinghaneyo <jinghaneyo@gmail.com>
2021-01-14 11:03:18 +0900

    패킷 Receive 처리 로직을 Callback 함수로 처리하기 위한 테스트 코드 추가

--- engine/eng/loan_analyzer/src/main.cpp
+++ engine/eng/loan_analyzer/src/main.cpp
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include "loan_Server.hpp"
 
+bool Event_Init()
+{
+	return true;
+}
+
+void Event_Close()
+{
+}
+
+void Event_Receive( __in char *_pData, __in size_t _nData_len )
+{
+	loan::MsgLog msgLog;
+	msgLog.ParseFromString(_pData);
+
+	if (false == msgLog.service_name().empty())
+		std::cout << "[Event_Receive] << Service Name : " << msgLog.service_name() << " << Log contents : " << msgLog.logcontents() << std::endl;
+}
+
 int main( int argc, char* argv[])
 {
     // google::InitGoogleLogging("DUMP");   
@@ -15,11 +33,20 @@ int main( int argc, char* argv[])
 
     std::cout << "[START] << server run (port = " << nPort << ")" << std::endl;
 
-	Rigitaeda::Rigi_TCPServerMgr<TCP_Session> mgr;
 	Rigitaeda::Rigi_Server server;
-
 	//server.Load_Conf();
+
+	/*
+	server.Add_Event_Handler_Close( Event_Close );
+	server.Add_Event_Handler_Init( Event_Init );
+	server.Add_Event_Handler_Receive( Event_Receive );
+	server.Run( nPort, 100 );
+	//*/
+
+	//*
+	Rigitaeda::Rigi_TCPServerMgr<TCP_Session> mgr;
 	server.Run( nPort, 100, &mgr );
+	//*/
 
     return 0;
 }
--- engine/gen_log.sh
+++ engine/gen_log.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
+if [ -f log.txt ];then
+	#rm -f $(pwd)/log.txt
+	rm -f log.txt
+fi
+
 count=0
 for ((;;))
 do
 	now=$(date)
 	now+=" >>  $count"
 	
-	echo "$now" >> "$(pwd)\log.txt"
+	#echo "$now" >> "$(pwd)\log.txt"
+	echo "$now" >> "log.txt"
 
 	count=$(expr $count + 1)
 
